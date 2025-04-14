73567ca74d217570ae465902aec1cd8a4ccfc4d2
Finnduas <88577279+Finnduas@users.noreply.github.com>
2025-04-14 21:01:20 +0200

    Created basic scaffolding for future changes(C#)
    Project started in C# with little to no code; just some code that reads a file and converts it to a string in memory and a class, that, for now at least, only prints out the code, but is supposed to be the entry point for the interpreter.

---
+++ InterpreterC#/Interpreter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterpreterC_
+{
+    internal class Interpreter
+    {
+        public void interpret(String code)
+        {
+            Console.WriteLine(code);
+            
+            Console.ReadLine(); 
+        }
+    }
+}
---
+++ InterpreterC#/Program.cs
@@ -0,0 +1,30 @@
+﻿using InterpreterC_;
+using System.Reflection.Metadata.Ecma335;
+
+String code;
+String line;
+
+    StreamReader sr = new("C:\\Users\\mikaa\\Desktop\\Tor Browser\\Browser\\TorBrowser\\Docs\\ChangeLog.monkey");
+
+    line = sr.ReadLine();
+    
+    if(line == null) {
+        Console.WriteLine("MainErr0: line == null <=> true => reading of file in specified directory failed ");
+        return;
+    }
+
+    code = line;
+    while (line != null)
+    {
+        line = sr.ReadLine();
+        code += "\n";
+        code += line;
+    }
+
+    sr.Close();
+
+    Interpreter interpreter = new();
+    if(code != null)
+    {
+        interpreter.interpret(code);
+    }
